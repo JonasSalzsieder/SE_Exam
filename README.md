b37c7510e1af6dca0be0364af4d2f7aacdc7f40b
Jonas Salzsieder <jonas.salzsieder@code.berlin>
2025-05-05 13:25:05 +0200

    Forgot to save my README before pushing it

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is my SE_01 Exam
+
+I came until task 2 to the very end but couldn't figure out where to put the variable of "no correct letter" = True:
--- hint.py
+++ hint.py
@@ -16,7 +16,7 @@ for letter in secret_word:
 for position in range(len(secret_word)):
     if user_input[position] == secret_word[position]:
         correct_positions.append(position)
-        print(correct_positions)
+        
 
 for index, position in enumerate(correct_positions):
     human_index = index +1
