cf3617dfca7c271ecefa1c452f886d7d9aea7bff
Jonas Salzsieder <jonas.salzsieder@code.berlin>
2025-05-05 13:01:34 +0200

    final commit

--- wordle.py
+++ wordle.py
@@ -23,30 +23,28 @@ while game_counter <= 5:
     elif len(user_input) == 5:
         print("Nice try.")
         guess_counter -= 1
-        
+            
+
         if guess_counter == 0:
-            print("Out of tries")
-            print(f"The word would have been {secret_word}")
-        
-
-
+                print("Out of tries")
+                print(f"The word would have been {secret_word}")
+            
         else:
-             
-            for letter in secret_word:
-                if letter in user_input:
-                    print(letter + " is in the word")
                 
-
-            for position in range(len(secret_word)):
-                correct_positions = []
-                correct_pos = correct_positions
-                if user_input[position] == secret_word[position]:
-                    correct_positions.append(position+1)
-                    print(f"You have a correct letter at position: {correct_pos}")
-            
-            
-                   
-            print(f"You have {guess_counter} tries left")
+                for letter in secret_word:
+                    if letter in user_input:
+                        print(letter + " is in the word")
+                    else:
+                        no_correct_letter = letter
+                    
+                for position in range(len(secret_word)):
+                    correct_positions = []
+                    correct_pos = correct_positions
+                    if user_input[position] == secret_word[position]:
+                        correct_positions.append(position+1)
+                        print(f"You have a correct letter at position: {correct_pos}")
+                
+                print(f"You have {guess_counter} tries left")
         game_counter += 1
     else:
         print("Better luck next time.")
